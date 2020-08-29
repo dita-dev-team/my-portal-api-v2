69eb1d61d210c83122babcc69876346a55d43102
Michael Bukachi <michaelbukachi@gmail.com>
2020-08-30 00:43:15 +0300

    feat: Setup koin for di

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val koin_version: String by project
 
 plugins {
     application
@@ -26,6 +24,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.koin:koin-ktor:$koin_version")
+    implementation("org.koin:koin-logger-slf4j:$koin_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,29 @@
 package dita.dev
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import org.koin.core.logger.Level
+import org.koin.ktor.ext.Koin
+import org.koin.logger.slf4jLogger
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.main(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Koin) {
+        slf4jLogger(Level.ERROR)
+        modules(appModules)
+    }
+
+    routing {
+        get("health_check") {
+            call.respondText("OK")
+        }
+    }
 }
 
