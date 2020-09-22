abc7fec001855c65c7020cffd57fca4ba3ceeebf
Michael Bukachi <michaelbukachi@gmail.com>
2020-09-22 22:56:43 +0300

    feat: Port sanitize method

--- src/utils/ExcelParser.kt
+++ src/utils/ExcelParser.kt
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class ExcelParser {
-    private var shift = ""
+    var shift = ""
     private var i = 0
     private var j = 0
 
@@ -15,6 +15,98 @@ class ExcelParser {
             else -> "X"
         }
         return code + section
+    }
+
+    fun sanitize(text: String): List<String> {
+        val coursesArray = mutableListOf<String>()
+        var courseCode = text.replace("\\s".toRegex(RegexOption.MULTILINE), "").replace("&".toRegex(), "/")
+        // Correct type AAA111B(X)
+        val match = "(.*)([A-Z]\\((.)\\))".toRegex().matchEntire(courseCode)
+        match?.let {
+            courseCode = it.groupValues[1] + it.groupValues[3]
+        }
+
+        val similarDoubleClasses =
+            "[a-z]{3,4}\\d{3}[a-z]/[a-z]{3,4}\\d{3}[a-z]*.*".toRegex(RegexOption.IGNORE_CASE) // YYY111A/YYY222A
+        val lackingClassPrefix =
+            "[a-z]{3,4}[\\d]{3}/[a-z]{3,4}[\\d]{3}[a-z]{1}".toRegex(RegexOption.IGNORE_CASE) // YYY111/YYY222A
+        val conjoinedClasses =
+            "[a-z]{3,4}[\\d]{3}[a-z]{1}/[a-z]{1}(?:[/]*|.{1})".toRegex(RegexOption.IGNORE_CASE) // YYY111A/B
+        val fourJoinedClasses =
+            "[A-Z]{3,4}[\\d]{3}(?:/[\\d]{3}[a-z]*)*".toRegex(RegexOption.IGNORE_CASE) // YYY111/222/333/444
+        val doubleJoined = "[A-Z]{3,4}/[A-Z]{3,4}[\\d]{3}[a-z]{1}".toRegex(RegexOption.IGNORE_CASE) // AAA/BBB111
+
+        coursesArray.add(courseCode)
+
+        if (courseCode.contains("/")) {
+            var courseCodes = emptyList<String>()
+            when {
+                courseCode.matches(similarDoubleClasses) -> {
+                    courseCodes = courseCode.split("/").map { code ->
+                        if (!code.last().isDigit()) code else addSection(code)
+                    }
+                }
+                courseCode.matches(lackingClassPrefix) -> {
+                    courseCodes = courseCode.split("/")
+                    courseCodes = courseCodes.mapIndexed { index, s ->
+                        if (index == 0) {
+                            s + courseCode.last()
+                        } else {
+                            s
+                        }
+                    }
+                }
+                courseCode.matches(conjoinedClasses) -> {
+                    val prefix = courseCode.substring(0, 6)
+                    val sections = courseCode.substring(6).split("/")
+                    courseCodes = sections.map {
+                        prefix + it
+                    }
+                }
+                courseCode.matches(doubleJoined) -> {
+                    courseCodes = courseCode.split("/")
+                    courseCodes = courseCodes.mapIndexed { index, s ->
+                        if (index == 0) {
+                            s + courseCodes[1].substring(3)
+                        } else {
+                            s
+                        }
+                    }
+                }
+                courseCode.matches(fourJoinedClasses) -> {
+                    val prefix = courseCode.substring(0, 3)
+                    val codes = courseCode.substring(3).split("/")
+                    val last = courseCode.last()
+                    courseCodes = codes.map { code ->
+                        val section: Char = if (!last.isDigit()) {
+                            last.toUpperCase()
+                        } else {
+                            when (shift) {
+                                "athi" -> 'A'
+                                "day" -> 'T'
+                                else -> 'X'
+                            }
+                        }
+                        prefix + code.substring(0, 3) + section
+                    }
+                }
+                else -> {
+                    println("Unable to sanitize $text")
+                }
+            }
+            val temp = mutableListOf<String>()
+            courseCodes.forEach { code ->
+                if (code.length > 7) {
+                    val chunks = chunkString(code, 7)
+                    temp.addAll(chunks)
+                } else {
+                    temp.add(code)
+                }
+            }
+            return temp
+        } else {
+            return coursesArray
+        }
     }
 
     fun stringToDate(text: String): Date? {
--- test/ExcelParserTest.kt
+++ test/ExcelParserTest.kt
@@ -1,4 +1,5 @@
 import dita.dev.utils.ExcelParser
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -8,16 +9,12 @@ import kotlin.test.assertNull
 class ExcelParserTest {
 
     @Test
-    fun `null is returned for invalid format`() {
+    fun `test stringToDate `() {
         val excelParser = ExcelParser()
-        val dateTime = excelParser.stringToDate("ksdfdsf")
+        var dateTime = excelParser.stringToDate("ksdfdsf")
         assertNull(dateTime)
-    }
 
-    @Test
-    fun `date object is returned`() {
-        val excelParser = ExcelParser()
-        var dateTime = excelParser.stringToDate("1/10/19 1:30pm")
+        dateTime = excelParser.stringToDate("1/10/19 1:30pm")
         assertThat(dateTime, instanceOf(Date::class.java))
 
         dateTime = excelParser.stringToDate("1/10/2019 1:30pm")
@@ -28,5 +25,37 @@ class ExcelParserTest {
 
         dateTime = excelParser.stringToDate("1/10/2019 1.30pm")
         assertThat(dateTime, instanceOf(Date::class.java))
+    }
+
+    @Test
+    fun `test sanitize returns units`() {
+        val excelParser = ExcelParser()
+        var result = excelParser.sanitize("ACS101A")
+        assertThat(result, `is`(listOf("ACS101A")))
+
+        result = excelParser.sanitize("ACS101A/B")
+        assertThat(result, `is`(listOf("ACS101A", "ACS101B")))
+
+        result = excelParser.sanitize("ACS101A/ACS101B")
+        assertThat(result, `is`(listOf("ACS101A", "ACS101B")))
+
+        excelParser.shift = "athi"
+        result = excelParser.sanitize("ACS101A/ACS201A/ACS102")
+        assertThat(result, `is`(listOf("ACS101A", "ACS201A", "ACS102A")))
+
+        excelParser.shift = ""
+        result = excelParser.sanitize("ACS101/ACS101B")
+        assertThat(result, `is`(listOf("ACS101B", "ACS101B")))
+
+        excelParser.shift = "athi"
+        result = excelParser.sanitize("ACS111/219/319/419")
+        assertThat(result, `is`(listOf("ACS111A", "ACS219A", "ACS319A", "ACS419A")))
+
+        excelParser.shift = ""
+        result = excelParser.sanitize("ACS111/219/319/419A")
+        assertThat(result, `is`(listOf("ACS111A", "ACS219A", "ACS319A", "ACS419A")))
+
+        result = excelParser.sanitize("ACS261/MIS224B")
+        assertThat(result, `is`(listOf("ACS261B", "MIS224B")))
     }
 }
